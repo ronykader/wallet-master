4c2be3aa54a06dab0f26d1288fb4600cdecc6f9d
ronykader <rony.ksr06@gmail.com>
2025-01-19 01:38:21 +0600

    added repository controller service and interface and full funcation for categoies

---
+++ app/Http/Controllers/API/v1/CategoryController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\v1\CategoryRequest;
+use App\Services\v1\CategoryService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function __construct(private readonly CategoryService $categoryService)
+    {
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        $categories = $this->categoryService->getAllCategories();
+        return response()->json($categories);
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request): JsonResponse
+    {
+        $category = $this->categoryService->createCategory($request->validated());
+        return response()->json(['message' => 'Category created successfully', 'data' => $category], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id): JsonResponse
+    {
+        $category = $this->categoryService->getCategoryById($id);
+        return response()->json($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryRequest $request, int $id): JsonResponse
+    {
+        $category = $this->categoryService->updateCategory($id, $request->validated());
+        return response()->json(['message' => 'Category updated successfully', 'data' => $category]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(int $id): JsonResponse
+    {
+        $this->categoryService->deleteCategory($id);
+        return response()->json(['message' => 'Category deleted successfully']);
+    }
+}
