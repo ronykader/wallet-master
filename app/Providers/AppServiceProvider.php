0c547014a7cc4fad4bcdb1038fc8950c21c81238
ronykader <rony.ksr06@gmail.com>
2025-01-29 01:05:42 +0600

    update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,20 @@
 
 namespace App\Providers;
 
+use App\Repositories\v1\AccountRepository;
+use App\Repositories\v1\BudgetRepository;
 use App\Repositories\v1\CategoryRepository;
+use App\Repositories\v1\FamilyMemberRepository;
+use App\Repositories\v1\interfaces\AccountRepositoryInterface;
+use App\Repositories\v1\interfaces\BudgetRepositoryInterface;
 use App\Repositories\v1\interfaces\CategoryRepositoryInterface;
+use App\Repositories\v1\interfaces\FamilyMemberRepositoryInterface;
+use App\Repositories\v1\interfaces\OrganizationRepositoryInterface;
+use App\Repositories\v1\interfaces\SubscriptionPlanRepositoryInterface;
+use App\Repositories\v1\interfaces\TransactionRepositoryInterface;
+use App\Repositories\v1\OrganizationRepository;
+use App\Repositories\v1\SubscriptionPlanRepository;
+use App\Repositories\v1\TransactionRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(AccountRepositoryInterface::class, AccountRepository::class);
+        $this->app->bind(BudgetRepositoryInterface::class, BudgetRepository::class);
+        $this->app->bind(FamilyMemberRepositoryInterface::class, FamilyMemberRepository::class);
+        $this->app->bind(OrganizationRepositoryInterface::class, OrganizationRepository::class);
+        $this->app->bind(SubscriptionPlanRepositoryInterface::class, SubscriptionPlanRepository::class);
+        $this->app->bind(TransactionRepositoryInterface::class, TransactionRepository::class);
     }
 
     /**
